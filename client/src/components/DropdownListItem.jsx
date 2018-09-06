567f6020f27c35c8be9f1d059491b4bc593d35d8
hifromkevin <lam.kevin86@gmail.com>
2018-09-06 09:04:12 -0700

    Improves dropdown styling

--- client/src/components/DropdownListItem.jsx
+++ client/src/components/DropdownListItem.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import FontAwesome from 'react-fontawesome';
 
 const DropdownListItem = ({title, switchDropdownSelected}) => (
-  <div className="" onClick={() => { switchDropdownSelected(title) }}>{title}</div>
+  <div onClick={() => { switchDropdownSelected(title) }}>{title}</div>
 );
 
 export default DropdownListItem;
